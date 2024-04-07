93ac8c45348b1da36b876aa4636f9ff59392bd0d
holasoymas <71905956331mahesh@gmail.com>
2024-04-07 08:44:00 +0545

    some minor change in hanoi program

--- recursion/tower_of_hanoi.c
+++ recursion/tower_of_hanoi.c
@@ -6,7 +6,7 @@ int main() {
   int n;
   printf("Enter the number of tower : \n");
   scanf("%d", &n);
-  towers(n, 'A', 'B', 'C');
+  towers(n, 'A', 'C', 'B');
   return 0;
 }
 
@@ -17,5 +17,5 @@ void towers(int n, char from, char to, char aux) {
   }
   towers(n - 1, from, aux, to);
   printf("Move disk %d from ring %c to ring %c\n", n, from, to);
-  towers(n - 1, aux, from, to);
+  towers(n - 1, aux, to, from);
 }
